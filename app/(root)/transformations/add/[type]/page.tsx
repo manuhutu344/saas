8c48842587a41ced6d5bc0c0d299cb76541af976
manuhutu344 <nandoperdos2@gmail.com>
2024-05-31 20:06:59 +0900

    Menambahkan form di dalam aplikasinya

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,11 +1,16 @@
 import Header from '@/components/shared/Header'
 import React from 'react'
 import { transformationTypes } from '@/constants'
+import TransformationForm from '@/components/shared/TransformationForm'
 
 function page({params:{type}}: SearchParamProps) {
   const transformation = transformationTypes[type]
   return (
-    <Header title={transformation.title} subtitle={transformation.subTitle} />
+    <>
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
+      <TransformationForm />
+    </>
+
   )
 }
 
